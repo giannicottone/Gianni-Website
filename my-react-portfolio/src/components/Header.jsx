3d721cff0fa1c17db71fd1afe2aa2491d3343c5b
Gianni Cottone <gjcottone@outlook.com>
2025-05-21 00:00:29 -0400

    Added Buttons that Do nothing, give the page a bit of structure

---
+++ my-react-portfolio/src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+export default function Header() {
+  return (
+    <section>
+        
+    <button>Gianni Cottone</button>
+    <button>Home</button>
+    <button>Projects</button>
+    <button>Contact</button>
+     
+    </section>
+  );
+}
+
+
+
